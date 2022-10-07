8e265703bc3c11137aec4ff438fb2585de2300d2
Natalie Lovibond Hodgson <90227438+smllygrl@users.noreply.github.com>
2022-10-07 14:52:59 +1100

    THE BIG BANG

---
+++ src/test/java/com/auspost/AddressesAPI/AddressesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auspost.AddressesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
